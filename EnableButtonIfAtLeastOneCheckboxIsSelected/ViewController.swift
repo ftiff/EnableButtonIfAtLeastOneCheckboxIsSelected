a833c6c0e47c0ea070fa77205b07386383a45751
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-05-23 22:51:20 +0300

    Initial Commit

---
+++ EnableButtonIfAtLeastOneCheckboxIsSelected/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EnableButtonIfAtLeastOneCheckboxIsSelected
+//
+//  Created by François Levaux on 23.05.17.
+//  Copyright © 2017 François Levaux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
